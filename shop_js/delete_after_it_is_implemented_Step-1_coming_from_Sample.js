ebb3aab8366978c23d6a831b556b08a9004c39a9
BibleFarm <smuglovsky@gmail.com>
2021-05-06 03:41:08 -0300

    .

--- shop_js/delete_after_it_is_implemented_Step-1_coming_from_Sample.js
+++ shop_js/delete_after_it_is_implemented_Step-1_coming_from_Sample.js
@@ -1,3 +1,141 @@
+////////////////////////// -->
+// BEGIN When coming from the changeVerse links ///// -->
+////////////////////////// -->
+$( ".changeVerse" ).on( "click", function() {
+// open the modal
+        $(".modal_customize_verse").show("slow");
+        $(".opacity_cover").show();
+// customer has selected a verse, so...
+     $('.lets_change').show();
+     $('.lets_choose').hide();
+$(".no_book_nor_chapter_nor_verse_selection_yet").hide();
+$(".verse_selection_complete").css("background", "#042400").show();
+var next_stepTimeoutID = setTimeout(function() {
+$(".next_step").css("background", "#292a00").show();
+  }, 2000);
+  setTimeout(function() {
+$(".verse_selection_complete").hide();
+  }, 4000);
+var willGrabBook = $('#hidden_book_desired').text();
+var willGrabChapter = $('#hidden_chapter_desired').text();
+var willGrabVerse = $('#hidden_verse_desired').text();
+var completeVerseURLtoGrab = ('https://team.biblefarm.org/_yournamehere_/' + willGrabBook + '-' + willGrabChapter + '-' + willGrabVerse + '.html');
+console.log("completeVerseURLtoGrab is: " + completeVerseURLtoGrab);
+// Assign handlers immediately after making the request,
+// and remember the jqxhr object for this request
+var jqxhr = $.get( completeVerseURLtoGrab, function() {
+  console.log( "Began bringing in the verse via Ajax" );
+})
+  .done(function(result) {
+var html = jQuery('<div>').html(result);
+var removedItalicsAndRef = $(html).find('.v').html().replace(/\(.*?\)/g , '').replace(/<i>/ , '').replace(/<\/i>/ , '');
+console.log("Removed the italics and ref like this: " + removedItalicsAndRef);
+$('#populated_ref_with_ajax').html(willGrabBook + ' ' + willGrabChapter + ' : ' + willGrabVerse);
+  console.log( 'Now populating the ref like this: ' + willGrabBook + ' ' + willGrabChapter + ' : ' + willGrabVerse);
+
+$('#populated_verse_with_ajax').html(removedItalicsAndRef);
+console.log(".done function is now populating the text of the verse...");
+
+////////////////////////// -->
+////////////////////////// -->
+// BEGIN render the Ajaxed verse
+////////////////////////// -->
+////////////////////////// -->
+	function renderIn_modalVerse() {
+		$('#populated_verse_with_ajax').html(
+			'<span>' + $('#populated_verse_with_ajax').text() + '</span>'
+		);
+		$('#populated_verse_with_ajax').textfill({
+			maxFontPixels: 200
+		});
+	}
+console.log("We're giving it 5 seconds before we renderIn_modalVerse.");
+setTimeout(function() {
+renderIn_modalVerse();
+}, 5000);
+setTimeout(function() {
+console.log("The 5 seconds went by, so we should now have it rendered.");
+}, 5000);
+// Update newly selected verse at the top
+var newlySelectedBook = $('#hidden_book_desired').text();
+var newlySelectedChapter = $('#hidden_chapter_desired').text();
+var newlySelectedVerse = $('#hidden_verse_desired').text();
+$(".selected_verse").text(newlySelectedBook + '-' + newlySelectedChapter + '-' + newlySelectedVerse);
+     $('.lets_choose').hide();
+     $('.lets_change').show();
+////////////////////////// -->
+////////////////////////// -->
+// END render the Ajaxed verse
+////////////////////////// -->
+////////////////////////// -->
+  })
+  .fail(function() {
+    console.log( ".fail function: Unable to retrieve that verse" );
+	$('#populated_verse_with_ajax').html("Nope. You selected the wrong chapter, or you selected the wrong verse. or you have no internet. Please try again.");
+// things went wrong, so we're back to square one
+	$(".selected_verse").text("Nothing selected yet");
+     $('.lets_choose').show();
+     $('.lets_change').hide();
+	$(".verse_selection_complete").hide();
+	$(".next_step").hide();
+	clearTimeout(next_stepTimeoutID);
+	$(".no_book_nor_chapter_nor_verse_selection_yet").show();
+	
+////////////////////////// -->
+////////////////////////// -->
+// BEGIN render the fail and error verse
+////////////////////////// -->
+////////////////////////// -->
+	function renderIn_modalVerse() {
+		$('#populated_verse_with_ajax').html(
+			'<span>' + $('#populated_verse_with_ajax').text() + '</span>'
+		);
+		$('#populated_verse_with_ajax').textfill({
+			maxFontPixels: 200
+		});
+	}
+console.log("We're giving it 5 seconds before we renderIn_modalVerse.");
+setTimeout(function() {
+renderIn_modalVerse();
+}, 5000);
+setTimeout(function() {
+console.log("The 5 seconds went by, so we should now have it rendered.");
+console.log( "Now things should be still okay, and ready to try again." );
+}, 5000);
+////////////////////////// -->
+////////////////////////// -->
+// END render the fail and error verse
+////////////////////////// -->
+////////////////////////// -->
+
+  })
+  .always(function() {
+    console.log( ".always function: Finished" );
+  });
+ 
+// Perform other work here ...
+ 
+// Set another completion function for the request above
+jqxhr.always(function() {
+  console.log( "jqxhr.always function: Second finished" );
+});
+});
+////////////////////////// -->
+// END When coming from the changeVerse links ///// -->
+////////////////////////// -->
+
+
+
+
+
+
+
+
+
+
+
+
+
 $( ".changeVerse" ).on( "click", function() {
 // close the menu_modal
         $(".menu_modal").hide("slow");
@@ -19,12 +157,10 @@ var booktoselect = $('#select_book option:contains("bookNeeded")').filter(functi
 $('#select_book option').removeClass('selected');
 //$('#select_book').trigger({type: 'click', which: 1});
 booktoselect.prop('selected', true).addClass('selected').trigger('change');
-
 var chaptertoselect = $('#select_chapter option:contains("chapterNeeded")').filter(function() {return $(this).text() === 'chapterNeeded';});
 $('#select_chapter option').removeClass('selected');
 //$('#select_chapter').trigger({type: 'click', which: 1});
 chaptertoselect.prop('selected', true).addClass('selected').trigger('change');
-
 var versetoselect = $('#select_verse option:contains("verseNeeded")').filter(function() {return $(this).text() === 'verseNeeded';});
 $('#select_verse option').removeClass('selected');
 //$('#select_verse').trigger({type: 'click', which: 1});
