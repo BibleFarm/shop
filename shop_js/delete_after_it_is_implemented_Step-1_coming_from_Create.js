af2d0df793970681281e1828309ff97df40953f4
BibleFarm <smuglovsky@gmail.com>
2021-05-06 00:03:12 -0300

    .

--- shop_js/delete_after_it_is_implemented_Step-1_coming_from_Create.js
+++ shop_js/delete_after_it_is_implemented_Step-1_coming_from_Create.js
@@ -1,3 +1,6 @@
+////////////////////////// -->
+// BEGIN When coming from the menu Create Your Own ///// -->
+////////////////////////// -->
 $( ".CreateYourOwnDesign" ).on( "click", function() {
 // close the menu_modal
         $(".menu_modal").hide("slow");
@@ -11,112 +14,74 @@ $( ".CreateYourOwnDesign" ).on( "click", function() {
      $('.lets_change').hide();
      $('.lets_choose').show();
 	 
-    ////////////////////////// -->
-    ////////////////////////// -->
-    // BEGIN render the sample verse
-    ////////////////////////// -->
-    ////////////////////////// -->
+////////////////////////// -->
+////////////////////////// -->
+// BEGIN render the welcome instructions
+////////////////////////// -->
+////////////////////////// -->
 	function renderIn_modalVerse() {
-		$('.selected_bible_verse_fixed_on_mug').html(
-			'<span>' + $('.selected_bible_verse_fixed_on_mug').text() + '</span>'
+		$('#populated_verse_with_ajax').html(
+			'<span>' + $('#populated_verse_with_ajax').text() + '</span>'
 		);
-		$('.selected_bible_verse_fixed_on_mug').textfill({
+		$('#populated_verse_with_ajax').textfill({
 			maxFontPixels: 200
 		});
 	}
+console.log("We're giving it 5 seconds before we renderIn_modalVerse.");
 setTimeout(function() {
 renderIn_modalVerse();
-}, 2000);
-
-    ////////////////////////// -->
-    ////////////////////////// -->
-    // END render the sample verse
-    ////////////////////////// -->
-    ////////////////////////// -->
+}, 5000);
+setTimeout(function() {
+console.log("The 5 seconds went by, so we should now have it rendered.");
+}, 5000);
+////////////////////////// -->
+////////////////////////// -->
+// END render the welcome instructions
+////////////////////////// -->
+////////////////////////// -->
 	 
  } else {
  }
 });
-
-
-
-  <!-- ////////////////////////// -->
-// initial book situation
-var selected_book = $('#select_book').find('.selected').html().split(' ')[0];
- console.log('Upon landing - Book: ' + selected_book);
-// initial chapter situation
-var selected_chapter = $('#select_chapter').find('.selected').html().split(' ')[0];
- console.log('Upon landing - Chapter: ' + selected_chapter);
-// initial verse situation
-var selected_verse = $('#select_verse').find('.selected').html().split(' ')[0];
- console.log('Upon landing - Verse: ' + selected_verse);
-// hidden containers holding selections get updated when page visited
-$('#hidden_book_desired').text(selected_book);
-$('#hidden_chapter_desired').text(selected_chapter);
-$('#hidden_verse_desired').text(selected_verse);
-  <!-- ////////////////////////// -->
-// update class on the select elements and update the hidden divs
-$('select#select_book').change(function(){
-    $(this).find(':selected').addClass('selected')
-           .siblings('option').removeClass('selected');
-var selected_book = $('#select_book').find('.selected').html().split(' ')[0];
-$('#hidden_book_desired').text(selected_book);
-});
-$('select#select_chapter').change(function(){
-    $(this).find(':selected').addClass('selected')
-           .siblings('option').removeClass('selected');
-var selected_chapter = $('#select_chapter').find('.selected').html().split(' ')[0];
-$('#hidden_chapter_desired').text(selected_chapter);
-});
-$('select#select_verse').change(function(){
-    $(this).find(':selected').addClass('selected')
-           .siblings('option').removeClass('selected');
-var selected_verse = $('#select_verse').find('.selected').html().split(' ')[0];
-		   $('#hidden_verse_desired').text(selected_verse);
-});
-  <!-- ////////////////////////// -->
-<!-- ////////////////////////// -->
-// BEGIN say "got it!" when all 3 are selected and brign in the scripture
-<!-- ////////////////////////// -->
+////////////////////////// -->
+// BEGIN say "got it!" when all 3 are selected and bring in the scripture
+////////////////////////// -->
 $('select#select_book, select#select_chapter, select#select_verse').change(function(){
-
+// if statement is to make sure all 3 select elements have been changed to something other than the defaults.
 if (($('#hidden_book_desired').text() != 'select') && ($('#hidden_chapter_desired').text() != '0') && ($('#hidden_verse_desired').text() != '0')) {
 $(".no_book_nor_chapter_nor_verse_selection_yet").hide();
 $(".verse_selection_complete").css("background", "#042400").show();
-  var next_stepTimeoutID = setTimeout(function() {
+var next_stepTimeoutID = setTimeout(function() {
 $(".next_step").css("background", "#292a00").show();
   }, 2000);
   setTimeout(function() {
 $(".verse_selection_complete").hide();
   }, 4000);
-
-// BEGIN Grab selected verse, show it on the mug, update the order, and texfill
 var willGrabBook = $('#hidden_book_desired').text();
 var willGrabChapter = $('#hidden_chapter_desired').text();
 var willGrabVerse = $('#hidden_verse_desired').text();
 var completeVerseURLtoGrab = ('https://team.biblefarm.org/_yournamehere_/' + willGrabBook + '-' + willGrabChapter + '-' + willGrabVerse + '.html');
-console.log(completeVerseURLtoGrab);
-
+console.log("completeVerseURLtoGrab is: " + completeVerseURLtoGrab);
 // Assign handlers immediately after making the request,
 // and remember the jqxhr object for this request
 var jqxhr = $.get( completeVerseURLtoGrab, function() {
-  console.log( "success" );
+  console.log( "Began bringing in the verse via Ajax" );
 })
   .done(function(result) {
 var html = jQuery('<div>').html(result);
-var removedItalicsAndRef = $(html).find('.v').html().replace(/\(.*?\)/g , '').replace(/<i>/ , '').replace(/<\/i>/ , '');   console.log(removedItalicsAndRef);
+var removedItalicsAndRef = $(html).find('.v').html().replace(/\(.*?\)/g , '').replace(/<i>/ , '').replace(/<\/i>/ , '');
+console.log("Removed the italics and ref like this: " + removedItalicsAndRef);
 $('#populated_ref_with_ajax').html(willGrabBook + ' ' + willGrabChapter + ' : ' + willGrabVerse);
-$('#populated_verse_with_ajax').html(removedItalicsAndRef);
+  console.log( 'Now populating the ref like this: ' + willGrabBook + ' ' + willGrabChapter + ' : ' + willGrabVerse);
 
-/*  setTimeout(function() {
-$('#populated_verse_with_ajax').textfill({maxFontPixels: 200});
-  }, 2000);*/
-  
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
-    // BEGIN render the sample verse
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
+$('#populated_verse_with_ajax').html(removedItalicsAndRef);
+console.log(".done function is now populating the text of the verse...");
+
+////////////////////////// -->
+////////////////////////// -->
+// BEGIN render the Ajaxed verse
+////////////////////////// -->
+////////////////////////// -->
 	function renderIn_modalVerse() {
 		$('#populated_verse_with_ajax').html(
 			'<span>' + $('#populated_verse_with_ajax').text() + '</span>'
@@ -125,60 +90,43 @@ $('#populated_verse_with_ajax').textfill({maxFontPixels: 200});
 			maxFontPixels: 200
 		});
 	}
-$( "#CreateYourOwnDesign, .changeVerse, #select_book, #select_chapter, #select_verse" ).on( "click change", function() {
-var checkIfDivIsVisible = setInterval(function() {
-   if (($('#populated_verse_with_ajax').length) && ($('#populated_verse_with_ajax').is(':visible'))) {
-      console.log("Upon Ajax .done populated_verse_with_ajax DIV has stuff & is visible, so we're rendering the textfill");
-	  	renderIn_modalVerse();
-      clearInterval(checkIfDivIsVisible);
-   }
-}, 100); // check every 100ms
-});
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
-    // END render the sample verse
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  setTimeout(function() {
-  $('#populated_verse_with_ajax');
-  }, 2000);
-  setTimeout(function() {
-$('#populated_verse_with_ajax');
-  }, 4000);
-    console.log( "second success" );
+console.log("We're giving it 5 seconds before we renderIn_modalVerse.");
+setTimeout(function() {
+renderIn_modalVerse();
+}, 5000);
+setTimeout(function() {
+console.log("The 5 seconds went by, so we should now have it rendered.");
+}, 5000);
+// Update newly selected verse at the top
+var newlySelectedBook = $('#hidden_book_desired').text();
+var newlySelectedChapter = $('#hidden_chapter_desired').text();
+var newlySelectedVerse = $('#hidden_verse_desired').text();
+$(".selected_verse").text(newlySelectedBook + '-' + newlySelectedChapter + '-' + newlySelectedVerse);
+     $('.lets_choose').hide();
+     $('.lets_change').show();
+////////////////////////// -->
+////////////////////////// -->
+// END render the Ajaxed verse
+////////////////////////// -->
+////////////////////////// -->
   })
   .fail(function() {
-    console.log( "unable to retrieve htat verse" );
+    console.log( ".fail function: Unable to retrieve that verse" );
 	$('#populated_verse_with_ajax').html("Nope. You selected the wrong chapter, or you selected the wrong verse. or you have no internet. Please try again.");
+// things went wrong, so we're back to square one
+	$(".selected_verse").text("Nothing selected yet");
+     $('.lets_choose').show();
+     $('.lets_change').hide();
 	$(".verse_selection_complete").hide();
 	$(".next_step").hide();
 	clearTimeout(next_stepTimeoutID);
 	$(".no_book_nor_chapter_nor_verse_selection_yet").show();
-
-
-/*  setTimeout(function() {
-$('#populated_verse_with_ajax').textfill({maxFontPixels: 200});
-  }, 2000);*/
-  
-  
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
-    // BEGIN render the sample verse
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
+	
+////////////////////////// -->
+////////////////////////// -->
+// BEGIN render the fail and error verse
+////////////////////////// -->
+////////////////////////// -->
 	function renderIn_modalVerse() {
 		$('#populated_verse_with_ajax').html(
 			'<span>' + $('#populated_verse_with_ajax').text() + '</span>'
@@ -187,52 +135,36 @@ $('#populated_verse_with_ajax').textfill({maxFontPixels: 200});
 			maxFontPixels: 200
 		});
 	}
-$( "#CreateYourOwnDesign, .changeVerse, #select_book, #select_chapter, #select_verse" ).on( "click change", function() {
-var checkIfDivIsVisible = setInterval(function() {
-   if (($('#populated_verse_with_ajax').length) && ($('#populated_verse_with_ajax').is(':visible'))) {
-      console.log("Upon Ajax .fail populated_verse_with_ajax DIV has stuff & is visible, so we're rendering the textfill");
-	  	renderIn_modalVerse();
-      clearInterval(checkIfDivIsVisible);
-   }
-}, 100); // check every 100ms
-});
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
-    // END render the sample verse
-  <!-- ////////////////////////// -->
-  <!-- ////////////////////////// -->
+console.log("We're giving it 5 seconds before we renderIn_modalVerse.");
+setTimeout(function() {
+renderIn_modalVerse();
+}, 5000);
+setTimeout(function() {
+console.log("The 5 seconds went by, so we should now have it rendered.");
+console.log( "Now things should be still okay, and ready to try again." );
+}, 5000);
+////////////////////////// -->
+////////////////////////// -->
+// END render the fail and error verse
+////////////////////////// -->
+////////////////////////// -->
 
-  
-  
-  
-  
-  
-  setTimeout(function() {
-  $('#populated_verse_with_ajax');
-  }, 2000);
-  setTimeout(function() {
-$('#populated_verse_with_ajax');
-  }, 4000);
-	
   })
   .always(function() {
-    console.log( "finished" );
+    console.log( ".always function: Finished" );
   });
  
 // Perform other work here ...
  
 // Set another completion function for the request above
 jqxhr.always(function() {
-  console.log( "second finished" );
+  console.log( "jqxhr.always function: Second finished" );
 });
-
-
-// END Grab selected verse, show it on the mug, update the order, and texfill
-}
-});
-<!-- ////////////////////////// -->
-// END say "got it!" when all 3 are selected and brign in the scripture
-<!-- ////////////////////////// -->
-
-
-
+} // this is the end of the if statement
+}); // this is the end of the change function
+////////////////////////// -->
+// END say "got it!" when all 3 are selected and bring in the scripture
+////////////////////////// -->
+////////////////////////// -->
+// END When coming from the menu Create Your Own ///// -->
+////////////////////////// -->
